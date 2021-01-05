0c8f5374123488b0350969e17677f1a1e98257cc
txxiao <txxiao92@163.com>
2021-01-05 23:51:53 +0800

    modified the wall picture

--- wall_painter.cpp
+++ wall_painter.cpp
@@ -6,10 +6,9 @@ WallPainter::WallPainter(int radius) : ScreenPainter(radius) {}
 
 void WallPainter::draw(QPainter& painter, int x, int y)
 {
-    QPen pen(Qt::gray);
-    QBrush brush(Qt::gray);
-    painter.setPen(pen);
-    painter.setBrush(brush);
+    QRect target(x - radius, y - radius, 2 * radius, 2 * radius);
+    QRect source(0.0, 0.0, 90, 89);
+    QImage image(":/res/images/wall.jpg");
 
-    painter.drawRect(x - radius, y - radius, 2 * radius, 2 * radius);
+    painter.drawImage(target, image, source);
 }
